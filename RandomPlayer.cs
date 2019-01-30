1d2d3ff8bb213a1080993a56b207a2af4adf9001
Jacky <cjacky624@gmail.com>
2019-01-30 08:47:36 -0500

    Initial commit

---
+++ RandomPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HangManStarterKit
+{
+    class RandomPlayer:Player
+    {
+        Random random = new Random();
+
+        public override char Guess()
+        {
+
+            char[] aToZ = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+            int randomNumber = random.Next(0, aToZ.Length);
+
+            return aToZ[randomNumber];
+        }
+    }
+}
